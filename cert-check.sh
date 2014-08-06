0c65d7d142071c81dc8c2e95f0f9e92fba0a12c0
Gerhard <info@tinned-software.net>
2014-08-06 12:12:36 +0200

    Added feature: chow chain by key and saving the chain

--- cert-check.sh
+++ cert-check.sh
@@ -2,7 +2,7 @@
 #
 # @author Gerhard Steinbeis (info [at] tinned-software [dot] net)
 # @copyright Copyright (c) 2014
-version=0.0.2
+version=0.1.0
 # @license http://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3
 # @package monitoring
 #
@@ -38,7 +38,15 @@ while [ $# -gt 0 ]; do
 			shift 2
 			;;
 
+		--chain-for-key)
+			CHAIN_FOR_KEY="YES"
+			shift 1
+			;;
 
+		--save-chain)
+			SAVE_CHAIN="YES"
+			shift 1
+			;;
 
 		# Unnamed parameter        
 		*)
@@ -78,9 +86,11 @@ if [ "$HELP" -eq "1" ]; then
     echo "Usage: `basename $0` [-hv] [--config filename.conf] certificate-file1.pem ... certificate-fileN.pem "
     echo "  -h  --help              Print this usage and exit"
     echo "  -v  --version           Print version information and exit"
-    echo "      --temp-path         Specify a directory where the script can create files."
+    echo "      --temp-path         Specify a directory where the script can create files"
     echo "                          This directory fill later contain the splitted PEM files with the"
-    echo "                          different certificates and keys seperated into single files."
+    echo "                          different certificates and keys seperated into single files"
+    echo "      --chain-for-key     Create the chain for the key (key needs to be provided in the files)"
+    echo "      --save-chain        Save the chain files as key, certificate and chain"
     echo 
     exit 1
 fi
@@ -98,6 +108,8 @@ DETECTED_OS_TYPE=`uname -s`
 rm -f $TEMP_PATH/*
 
 # for each file do
+echo 
+echo -n "*** Splitting up files into certificates and keys ..."
 for FILE in $INPUT_FILE_LIST
 do
 	# Get the number of parts in this file
@@ -111,13 +123,13 @@ do
 
 	# if only one certificate in the file, copy it over
 	if [[ "$REPEAT_COUNT" -lt "0" ]]; then
-		echo "*** DBG: $FILE with $PART_COUNT parts ... copy"
+		#echo "*** DBG: $FILE with $PART_COUNT parts ... copy"
 		cp "$FILE" "$TEMP_PATH/${FILE_BASENAME}_part00"
 		continue
 	fi
 
 	# OS specific parameters to execurte csplit
-	echo "*** DBG: $FILE with $PART_COUNT parts ... csplit ($DETECTED_OS_TYPE)"
+	#echo "*** DBG: $FILE with $PART_COUNT parts ... csplit ($DETECTED_OS_TYPE)"
 	case $DETECTED_OS_TYPE in 
 		Linux)
 			csplit --elide-empty-files -s -f $TEMP_PATH/${FILE_BASENAME}_part $FILE '/-----BEGIN/' '{*}'
@@ -128,7 +140,9 @@ do
 	esac
 done
 
+
 FLC=0
+KEY_LIST=''
 for FILE in $TEMP_PATH/*; do
 	TYPE=`grep -h "\-\-\-\-\-BEGIN" "$FILE" | sed 's/^.* \([A-Z]*\).*$/\1/'`
 	mv "$FILE" "${FILE}-$TYPE"
@@ -156,6 +170,7 @@ for FILE in $TEMP_PATH/*; do
 			FILE_HASH[$FLC]='-'
 			FILE_ISSUER_HASH[$FLC]='-'
 			FILE_SUBJECT[$FLC]='-'
+			KEY_LIST="$KEY_LIST $FLC"
 		else
 			continue
 		fi
@@ -163,6 +178,7 @@ for FILE in $TEMP_PATH/*; do
 
 	FLC=$((FLC + 1))
 done
+echo " $FLC items found"
 
 # Find the child/parent relation between the certificates and match the keys
 for (( i = 0; i < $FLC; i++ )); do
@@ -176,7 +192,7 @@ for (( i = 0; i < $FLC; i++ )); do
 			# find matching issuer certificate
 			if [[ "${FILE_SIG_KEY_ID[$i]}" == "${FILE_KEY_ID[$a]}" ]] && [[ "${FILE_ISSUER_HASH[$i]}" == "${FILE_HASH[$a]}" ]]; then
 				FILE_PARENT[$i]=$a;
-				FILE_CHILDS[$a]="${FILE_CHILDS[$a]} $i "
+				FILE_CHILDS[$a]="${FILE_CHILDS[$a]}$i "
 			fi
 		done
 	fi
@@ -184,7 +200,8 @@ for (( i = 0; i < $FLC; i++ )); do
 		for (( j = 0; j < $FLC; j++ )); do
 			if [[ "${FILE_PUB_KEY[$i]}" == "${FILE_PUB_KEY[$j]}" ]] && [[ "${FILE_TYPE[$j]}" == "CERT" ]]; then
 				KEY_ASSIGNMENT[$j]=$i;
-				echo "*** DBG: Matching key found: Cert: $j match to Key $i."
+				FILE_PARENT[$i]=$j;
+				FILE_CHILDS[$j]="${FILE_CHILDS[$j]}$i "
 			fi
 		done
 	fi
@@ -198,134 +215,153 @@ for (( i = 0; i < $FLC; i++ )); do
 done
 
 
-
-
-for (( i = 0; i < $FLC; i++ )); do
-	echo "*** DBG:  $i: ${FILE_LIST[$i]}"
-	echo "*** DBG:               type: ${FILE_TYPE[$i]} ${FILE_NOTICE[$i]}"
-	echo "*** DBG:            subject: ${FILE_SUBJECT[$i]}"
-	echo "*** DBG:        fingerprint: ${FILE_FINGERPRINT[$i]}"
-	echo "*** DBG:             serial: ${FILE_SERIAL[$i]}"
-	echo "*** DBG:          notBefore: ${FILE_DATE_START[$i]}"
-	echo "*** DBG:           notAfter: ${FILE_DATE_END[$i]}"
-	echo "*** DBG:         public-key: ${FILE_PUB_KEY[$i]}"
-	echo "*** DBG:               hash: ${FILE_HASH[$i]}"
-	echo "*** DBG:             issuer: ${FILE_ISSUER[$i]}"
-	echo "*** DBG:        issuer-hash: ${FILE_ISSUER_HASH[$i]}"
-	echo "*** DBG:             key-id: ${FILE_KEY_ID[$i]}"
-	echo "*** DBG:   signature-key-id: ${FILE_SIG_KEY_ID[$i]}"
-	echo "*** DBG:        parent-cert: ${FILE_PARENT[$i]}"
-	echo "*** DBG:        child-certs: ${FILE_CHILDS[$i]}"
-done
-echo "*** DBG:  ROOT_LIST: $ROOT_LIST"
-
+#for (( i = 0; i < $FLC; i++ )); do
+#	echo "*** DBG:  $i: ${FILE_LIST[$i]}"
+#	echo "*** DBG:               type: ${FILE_TYPE[$i]} ${FILE_NOTICE[$i]}"
+#	echo "*** DBG:            subject: ${FILE_SUBJECT[$i]}"
+#	echo "*** DBG:        fingerprint: ${FILE_FINGERPRINT[$i]}"
+#	echo "*** DBG:             serial: ${FILE_SERIAL[$i]}"
+#	echo "*** DBG:          notBefore: ${FILE_DATE_START[$i]}"
+#	echo "*** DBG:           notAfter: ${FILE_DATE_END[$i]}"
+#	echo "*** DBG:         public-key: ${FILE_PUB_KEY[$i]}"
+#	echo "*** DBG:               hash: ${FILE_HASH[$i]}"
+#	echo "*** DBG:             issuer: ${FILE_ISSUER[$i]}"
+#	echo "*** DBG:        issuer-hash: ${FILE_ISSUER_HASH[$i]}"
+#	echo "*** DBG:             key-id: ${FILE_KEY_ID[$i]}"
+#	echo "*** DBG:   signature-key-id: ${FILE_SIG_KEY_ID[$i]}"
+#	echo "*** DBG:        parent-cert: ${FILE_PARENT[$i]}"
+#	echo "*** DBG:        child-certs: ${FILE_CHILDS[$i]}"
+#done
+#echo "*** DBG:  ROOT_LIST: $ROOT_LIST"
 
 
 
-
-function print_certificates()
+#
+# Function to print the complete certificate chain
+#
+function print_certificate_details()
 {
 	local INTEND="$1"
-	shift
-	ITEM_LIST=$@
+	local k=$2
 
-	for k in $ITEM_LIST; do
+	if [[ "${FILE_TYPE[$k]}" == "CERT" ]]
+	then
 		echo "${INTEND}Certificate file          : ${FILE_LIST[$k]} (internal-id: $k)"
 		echo "${INTEND}Certificate subject       : ${FILE_SUBJECT[$k]} (hash: ${FILE_HASH[$k]})"
 		echo "${INTEND}Certificate serial        : ${FILE_SERIAL[$k]} "
 		echo "${INTEND}Certificate Key Identifier: ${FILE_KEY_ID[$k]}"
+		echo "${INTEND}Certificate Key hash      : ${FILE_PUB_KEY[$k]}"
 		echo "${INTEND}Issuer Subject            : ${FILE_ISSUER[$k]} (issuer hash: ${FILE_ISSUER_HASH[$k]})"
 		echo "${INTEND}Issuer Key Identifier     : ${FILE_SIG_KEY_ID[$k]}"
-		echo "${INTEND}*** DBG: parent-cert      : ${FILE_PARENT[$k]} / child-certs: ${FILE_CHILDS[$k]}"
-		if [[ "${KEY_ASSIGNMENT[$k]}" -ne "" ]]; then
-			KI=${KEY_ASSIGNMENT[$k]}
-			echo "${INTEND}***"
-			echo "${INTEND}*** Matching Key          : ${FILE_LIST[$KI]}"
-			echo "${INTEND}*** DBG: key-parent-cert  : $k"
-		fi
-		echo ""
+		echo "${INTEND}Parent item in chain      : ${FILE_PARENT[$k]}"
+		echo "${INTEND}Child item in chain       : ${FILE_CHILDS[$k]}"
+	fi
 
+	if [[ "${FILE_TYPE[$k]}" == "KEY" ]]
+	then
+		echo "${INTEND}Key file                  : ${FILE_LIST[$k]} (internal-id: $k)"
+		echo "${INTEND}Key hash                  : ${FILE_PUB_KEY[$k]}"
+		echo "${INTEND}Parent item in chain      : ${FILE_PARENT[$k]}"
+	fi
+
+	echo ""
+}
+
+
+#
+# Function to print the complete certificate chain
+#
+function print_certificates()
+{
+	local INTEND="$1"
+	shift
+	local ITEM_LIST=$@
+
+	for k in $ITEM_LIST; do
+		print_certificate_details "$INTEND" "$k"
 		print_certificates "$INTEND    " ${FILE_CHILDS[$k]}
-
 	done
 
 }
 
-
-print_certificates '' $ROOT_LIST
-
-
-
-
-## print the certificate tree and the key matching
-#INTEND=''
-#for k in $RESULT_ORDER; do
-#	echo "${INTEND}Certificate file: ${FILE_LIST[$k]} (internal-id: $k)"
-#	echo "${INTEND}Certificate hash: ${FILE_HASH[$k]} (issuer: ${FILE_ISSUER_HASH[$k]})"
-#	SUBJECT=`echo "${FILE_SUBJECT[$k]}" | sed 's/^subject= //'`
-#	echo "${INTEND}Certificate subj: $SUBJECT"
-#	if [[ "${KEY_ASSIGNMENT[$k]}" -ne "" ]]; then
-#		KI=${KEY_ASSIGNMENT[$k]}
-#		echo "${INTEND}***"
-#		echo "${INTEND}*** Matching Key: ${FILE_LIST[$KI]}"
-#	fi
-#	echo ""
 #
-#	INTEND="$INTEND        "
-#done
-#echo ""
+# Print the chain for one item by checking the parent relation (also saving the chain)
+#
+function print_chain_for_item()
+{
+	local INTEND="$1"
+	local ITEM="$2"
+
+	# find the first parent 
+	ITEM_PARENT=${FILE_PARENT[$ITEM]}
+
+	# start the certificate path for saving the certificate chain into a file
+	if [[ "${FILE_TYPE[$ITEM]}" == "CERT" ]]
+	then
+		# if the start item is a certificate, then there is no key available for it
+		ITEM_PATH_SAVE="$ITEM_PARENT"
+		ITEM_PATH_SAVE_KEY=""
+		ITEM_PATH_SAVE_CERT="$ITEM"
+	else
+		# if the start item is a key define the key and the first parent as the certificate
+		ITEM_PATH_SAVE=""
+		ITEM_PATH_SAVE_KEY="$ITEM"
+		ITEM_PATH_SAVE_CERT="$ITEM_PARENT"
+	fi
+	
+	# start the chain to display including the key and the certificate
+	ITEM_PATH="$ITEM_PARENT $ITEM"
+
+	# get through the parent relationship to find the root element
+	while [[ $ITEM_PARENT != '' ]]
+	do
+		ITEM_PARENT=${FILE_PARENT[$ITEM_PARENT]}
+		ITEM_PATH="$ITEM_PARENT $ITEM_PATH"
+		# ignore the root element for the certificate chain
+		if [[ $ITEM_PARENT != '' ]]
+		then
+			# if there is a parent item, add the item to the chain
+			ITEM_PATH_SAVE="$ITEM_PATH_SAVE $ITEM_PARENT"
+		fi
+		
+	done
+
+	# show the certificate chain
+	for i in $ITEM_PATH
+	do
+		print_certificate_details "$INTEND" "$i"
+		INTEND="$INTEND    "
+	done
+
+	# save the certificate chain in reverse order and without the root certificate
+	if [[ "$SAVE_CHAIN" = "YES" ]]
+	then
+		for j in $ITEM_PATH_SAVE
+		do
+			rm -f "$TEMP_PATH/result-chain.pem"
+			rm -f "$TEMP_PATH/result-cert.pem"
+			rm -f "$TEMP_PATH/result-key.pem"
+			cat "${FILE_LIST[$j]}" >>"$TEMP_PATH/result-chain.pem"
+			cat "${FILE_LIST[$ITEM_PATH_SAVE_CERT]}" >>"$TEMP_PATH/result-cert.pem"
+			cat "${FILE_LIST[$ITEM_PATH_SAVE_KEY]}" >>"$TEMP_PATH/result-key.pem"
+		done
+	fi
+}
 
 
-
-
-
-# TEMP COMMANDS
-#
-# openssl asn1parse -in temp/geotrust_CA-cert.pem_part00-CERTIFICATE
-#
-#
-#
-#
-
-
-#
-# LINK LIST
-#
-# http://www.openssl.org/docs/apps/rsautl.html
-# 	- rsauthl seems to be able to show the certificate-signature details
-#		http://openssl.6102.n7.nabble.com/verify-signature-using-public-key-td12297.html
-#
-# http://www.herongyang.com/Cryptography/OpenSSL-Certificate-Path-Validation-Tests.html
-# 	- Verify command usage
 # 
-# http://www.herongyang.com/Cryptography/OpenSSL-Certificate-Path-Create-Sample-Certificates.html
-# 	- Create CA and certificate path for testing
+# Show the result according to the requested parameters
 # 
-# http://www.tinned-software.net//demo/rfc-viewer/rfcview.php?number=5280&loc=remote#page71
-# 	- Certificate verification RFC
-# 
-# http://www.cyberciti.biz/faq/test-ssl-certificates-diagnosis-ssl-certificate/
-# 	- get certificate from remote
-# 
+if [[ -z "$CHAIN_FOR_KEY" ]]
+then
+	print_certificates '' $ROOT_LIST
+else
+	for i in "$KEY_LIST"; do
+		echo "========================="
+		print_chain_for_item '' "$i"
+		echo "========================="
+		echo 
+	done
+fi
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+exit 0
